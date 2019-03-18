273c5d7e72349abd3d745ef388c18d5707198d6f
omgjrswtf <johnrex.salvador3013@gmail.com>
2019-03-18 08:31:05 +0800

    test db

--- core/database/connection.php
+++ core/database/connection.php
@@ -1,8 +1,13 @@
 <?php 
-	$dsn = 'mysql:host=localhost; dbname=bscndb';
-	$user = 'root';
-	$pass = '';
+	// $dsn = 'mysql:host=localhost; dbname=bscndb';
+	// $user = 'root';
+	// $pass = '';
 
+	$dsn = 'mysql:host=helplyphbetainstance.cdvjflsxgq7d.us-east-2.rds.amazonaws.com; dbname=helplyph_db';
+	$user = 'helply_ph';
+	$pass = 'm4rKJAn3';
+
+	
 	try {
 		$pdo = new PDO($dsn, $user, $pass);
 	} catch (PDOException $e) {
