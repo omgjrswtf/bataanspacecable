5488a8d6f2137d27393d860ce648b52cf48568dd
omgjrswtf <johnrex.salvador3013@gmail.com>
2019-03-25 03:03:56 +0800

    datebase upload

--- core/database/connection.php
+++ core/database/connection.php
@@ -1,11 +1,11 @@
 <?php 
-	$dsn = 'mysql:host=localhost; dbname=bscndb';
-	$user = 'root';
-	$pass = '';
+	// $dsn = 'mysql:host=localhost; dbname=bscndb';
+	// $user = 'root';
+	// $pass = '';
 
-	// $dsn = 'mysql:host=helplyphbetainstance.cdvjflsxgq7d.us-east-2.rds.amazonaws.com; dbname=helplyph_db';
-	// $user = 'helply_ph';
-	// $pass = 'm4rKJAn3';
+	$dsn = 'mysql:host=helplyphbetainstance.cdvjflsxgq7d.us-east-2.rds.amazonaws.com; dbname=helplyph_db';
+	$user = 'helply_ph';
+	$pass = 'm4rKJAn3';
 
 	
 	try {
