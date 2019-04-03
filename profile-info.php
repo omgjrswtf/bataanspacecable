e6cb8557c5845f24c006d8183bcae5c4e8623071
keiths09 <kinekimalone@gmail.com>
2019-04-04 03:50:45 +0800

    Update Profile Info 2
    Update Profile Info 2

--- profile-info.php
+++ profile-info.php
@@ -88,7 +88,7 @@
               &nbsp;&nbsp;&nbsp;
               <?php echo "<b>&#x205E; Welcome </b>". $client->getGender(). " $client->fname $client->lname"; ?>
               </div>
-              <button onclick="history.go(-1);" style="float: right; margin-top: -25px; color: white;" class="btn btn-primary"><span class="glyphicon glyphicon-arrow-left"></span> Back</button>
+              <button onclick="history.go(-1);" style="float: right; color: white;" class="btn btn-primary"><span class="glyphicon glyphicon-arrow-left"></span> Back</button>
             </div>
           </div>
         </div>
